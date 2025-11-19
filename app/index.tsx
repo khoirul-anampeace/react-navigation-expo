ae4587ec6ed5c5332e0b70d9dfa0cffb031c94de
ANAM <khoirul130203@gmail.com>
2025-11-19 23:38:22 +0700

    add loading at index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '../src/context/ThemeContext';
 import RootNavigator from '../src/navigation/RootNavigator';
+import LoginScreen from '../src/screens/auth/LoginScreen';
+import { useAppSelector } from '../src/store/hooks';
 import { store } from '../src/store/store';
+
+function AppContent() {
+  const { isAuthenticated, isLoading } = useAppSelector((state) => state.auth);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#2196F3" />
+      </View>
+    );
+  }
+
+  return isAuthenticated ? <RootNavigator /> : <LoginScreen />;
+}
 
 export default function Index() {
   return (
     <Provider store={store}>
       <ThemeProvider>
-        <RootNavigator />
+        <AppContent />
       </ThemeProvider>
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+});
