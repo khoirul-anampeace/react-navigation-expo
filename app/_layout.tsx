9cb0d3d02fdc6099b3284f7915d70991c9da063e
ANAM <khoirul130203@gmail.com>
2025-12-04 13:02:53 +0700

    fixing iconbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,29 @@
+
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import Toast from 'react-native-toast-message';
 import { toastConfig } from '../src/config/toastConfig';
+import { ThemeProvider, useTheme } from '../src/context/ThemeContext';
 
-export default function RootLayout() {
+
+
+function LayoutInner() {
+  const { isDarkMode } = useTheme();
   return (
     <>
+      <StatusBar style={isDarkMode ? 'light' : 'dark'} />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
       </Stack>
       <Toast config={toastConfig} />
     </>
+  );
+}
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider>
+      <LayoutInner />
+    </ThemeProvider>
   );
 }
